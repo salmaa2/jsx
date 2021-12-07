f24e8f12ba8a8b55ae5ea9568fe35da205be6748
salma chebbi <salmaaa.shebby@gmail.com>
2021-12-07 19:00:03 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import image  from './imgsrc.jpg'
+
+function Convert() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+        <h1 className="title red">Votre nom ici</h1>
+        <br />
+        <img src={image} />
+        <br />
+        <img src="/imgpublic.jpg" />
+      </div>
+      <vidéo width={320} height={240} contrôles>
+        <source src="maVidéo.mp4" type="video/mp4" />
+      </vidéo>
     </div>
   );
 }
 
-export default App;
+export default Convert
